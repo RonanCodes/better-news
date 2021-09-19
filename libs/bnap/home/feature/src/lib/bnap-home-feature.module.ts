11c2307ab8aac1ceb9fcc9353a21b61d7cb55473
Ronan Connolly <Ronan@RonanConnolly.dev>
2021-09-19 18:41:08 +0100

    Remove dep

--- libs/bnap/home/feature/src/lib/bnap-home-feature.module.ts
+++ libs/bnap/home/feature/src/lib/bnap-home-feature.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { BnapSharedUiArticleModule } from '@better-news/bnap/shared/ui/article';
 
 @NgModule({
   imports: [
@@ -10,7 +9,6 @@ import { BnapSharedUiArticleModule } from '@better-news/bnap/shared/ui/article';
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: HomeComponent },
     ]),
-    BnapSharedUiArticleModule,
     RouterModule,
   ],
   declarations: [HomeComponent],
