966d4f0d43821f8656b623e6cf5e2b03b70eadae
Ronan Connolly <Ronan@RonanConnolly.dev>
2021-09-19 18:18:16 +0100

    Sort out header and home link

--- apps/better-news-app/src/app/app.component.ts
+++ apps/better-news-app/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'bnap-root',
-  template: '<router-outlet></router-outlet>',
-  styleUrls: ['app.styles.scss'],
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.styles.scss'],
 })
 export class AppComponent {}
